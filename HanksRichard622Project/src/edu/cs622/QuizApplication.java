8e1f7604155dd1de2f6c05c34103e12d19da9bb2
Richard Hanks <richardhanksjr@gmail.com>
2018-05-30 16:59:35 -0400

    Can now refresh the screen with new questions

--- HanksRichard622Project/src/edu/cs622/QuizApplication.java
+++ HanksRichard622Project/src/edu/cs622/QuizApplication.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -19,10 +18,12 @@ import javafx.stage.Stage;
 public class QuizApplication extends Application{
 // Declare an instance of a stage globally so that I can modify it throughout
 	private Stage stage;
+	Question question;
+	static String username;
+	Text questionText = new Text();
 	@Override
-	
 	 public void start(Stage mainStage){
-			stage = mainStage;
+		stage = mainStage;
 		mainStage.setTitle("MusicTrivia");
 		Scene userNameScene = getLoginScene();
 		stage.setScene(userNameScene);
@@ -31,6 +32,7 @@ public class QuizApplication extends Application{
 	} 
 	
 	public static void main(String[] args){
+		// Generate the initial questions
 		launch(args);
 	}
 	
@@ -47,7 +49,7 @@ public class QuizApplication extends Application{
 		grid.add(userNameLabel, 0, 1);
 		TextField userNameText = new TextField();
 		grid.add(userNameText, 1, 1);
-		grid.add(scenetitle, 0, 0, 1, 1);
+		grid.add(scenetitle, 0,0,1, 1);
 		// Submit button
 		Button btn = new Button("Sign in");
 		HBox hbBtn = new HBox(10);
@@ -61,8 +63,8 @@ public class QuizApplication extends Application{
 			@Override
 			public void handle(ActionEvent e){
 				try {
-					String username = userNameText.getText();
-					setGameScene(username);
+					QuizApplication.username = userNameText.getText();
+					setGameScene();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -72,7 +74,7 @@ public class QuizApplication extends Application{
 		return userNameScene;
 	}
 	
-	private void setGameScene(String username) throws Exception{
+	private void setGameScene() throws Exception{
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
@@ -81,10 +83,64 @@ public class QuizApplication extends Application{
 		Scene gameScene = new Scene(grid, 400, 275);
 		Text scenetitle = new Text("Let's play, " + username + "!");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 0, 0, 1, 1);
+		question = (AbstractQuestion) Quiz.getQuestion();
+		grid.add(scenetitle, 2,0);
+		Text questionText = new Text(question.getQuestion());
+		questionText.wrappingWidthProperty().bind(gameScene.widthProperty().subtract(30));
+		grid.add(questionText, 2, 2);
+		Button quesBtn = new Button("Next Question");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BASELINE_CENTER);
+		hbBtn.getChildren().add(quesBtn);
+		grid.add(hbBtn, 2, 4);
 		stage.setScene(gameScene);
-	}
+		quesBtn.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e){
+				System.out.println("question is: " + question.getQuestion());
+				try {
+					setGameScene();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
 			
+		});
+
+
+	}
+	
+//	private Scene updateGameScene(){
+//		GridPane grid = new GridPane();
+//		grid.setAlignment(Pos.CENTER);
+//		grid.setHgap(10);
+//		grid.setVgap(10);
+//		grid.setPadding(new Insets(25, 25, 25, 25));
+////		Scene gameScene = new Scene(grid, 400, 275);
+////		Text scenetitle = new Text("Let's play, " + username + "!");
+////		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+////		grid.add(scenetitle, 0, 0, 1, 1);
+//		// Get the next question to ask
+////		Button quesBtn = new Button("Next Question");
+//		HBox hbBtn = new HBox(10);
+//		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+////		hbBtn.getChildren().add(quesBtn);
+//		grid.add(hbBtn, 1, 4);
+//		//Text for the question
+////		quesBtn.setOnAction(new EventHandler<ActionEvent>(){
+////			@Override
+////			public void handle(ActionEvent e){
+////				question = (AbstractQuestion) Quiz.getQuestion();
+//////				questionText = new Text(question.getQuestion());
+////				questionText.setText(question.getQuestion());
+////				grid.add(questionText, 2, 2);
+////				System.out.println("question is: " + question.getQuestion());
+////			}
+////		});
+//		return gameScene;
+//	}
+				
 }
 
 
