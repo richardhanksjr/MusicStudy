34e667c9fcc7d974cabdd2a2e61043269670f9d7
Richard Hanks <richardhanksjr@gmail.com>
2018-05-30 21:57:01 -0400

    Gui is now working through the old flow, but no multiple choice options

--- HanksRichard622Project/src/edu/cs622/QuizApplication.java
+++ HanksRichard622Project/src/edu/cs622/QuizApplication.java
@@ -20,6 +20,7 @@ public class QuizApplication extends Application{
 	private Stage stage;
 	Question question;
 	static String username;
+	private static User user;
 	Text questionText = new Text();
 	@Override
 	 public void start(Stage mainStage){
@@ -63,7 +64,8 @@ public class QuizApplication extends Application{
 			@Override
 			public void handle(ActionEvent e){
 				try {
-					QuizApplication.username = userNameText.getText();
+					QuizApplication.user = new User(userNameText.getText());
+					System.out.println("user scores: " + QuizApplication.user.getScores());
 					setGameScene();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
@@ -74,6 +76,11 @@ public class QuizApplication extends Application{
 		return userNameScene;
 	}
 	
+	/**
+	 * This is called after "login" and when user selects "next question" to load the next question along with a selection of "multiple choice" answers.
+	 * 
+	 * @throws Exception
+	 */
 	private void setGameScene() throws Exception{
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
@@ -81,25 +88,24 @@ public class QuizApplication extends Application{
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		Scene gameScene = new Scene(grid, 400, 275);
-		Text scenetitle = new Text("Let's play, " + username + "!");
+		Text scenetitle = new Text("Let's play, " + user.getUserName() + "!");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		question = (AbstractQuestion) Quiz.getQuestion();
 		grid.add(scenetitle, 2,0);
 		Text questionText = new Text(question.getQuestion());
 		questionText.wrappingWidthProperty().bind(gameScene.widthProperty().subtract(30));
 		grid.add(questionText, 2, 2);
-		Button quesBtn = new Button("Next Question");
+		Button correctQuesBtn = new Button(question.getAnswer());
 		HBox hbBtn = new HBox(10);
 		hbBtn.setAlignment(Pos.BASELINE_CENTER);
-		hbBtn.getChildren().add(quesBtn);
+		hbBtn.getChildren().add(correctQuesBtn);
 		grid.add(hbBtn, 2, 4);
 		stage.setScene(gameScene);
-		quesBtn.setOnAction(new EventHandler<ActionEvent>(){
+		correctQuesBtn.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent e){
-				System.out.println("question is: " + question.getQuestion());
 				try {
-					setGameScene();
+					setAnswerScene(true);
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -107,39 +113,89 @@ public class QuizApplication extends Application{
 			}
 			
 		});
-
-
 	}
 	
-//	private Scene updateGameScene(){
-//		GridPane grid = new GridPane();
-//		grid.setAlignment(Pos.CENTER);
-//		grid.setHgap(10);
-//		grid.setVgap(10);
-//		grid.setPadding(new Insets(25, 25, 25, 25));
-////		Scene gameScene = new Scene(grid, 400, 275);
-////		Text scenetitle = new Text("Let's play, " + username + "!");
-////		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-////		grid.add(scenetitle, 0, 0, 1, 1);
-//		// Get the next question to ask
-////		Button quesBtn = new Button("Next Question");
-//		HBox hbBtn = new HBox(10);
-//		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-////		hbBtn.getChildren().add(quesBtn);
-//		grid.add(hbBtn, 1, 4);
-//		//Text for the question
-////		quesBtn.setOnAction(new EventHandler<ActionEvent>(){
-////			@Override
-////			public void handle(ActionEvent e){
-////				question = (AbstractQuestion) Quiz.getQuestion();
-//////				questionText = new Text(question.getQuestion());
-////				questionText.setText(question.getQuestion());
-////				grid.add(questionText, 2, 2);
-////				System.out.println("question is: " + question.getQuestion());
-////			}
-////		});
-//		return gameScene;
-//	}
+	// Called when an answer is selected.  Displays feedback to the user on their answer.  Gives option to quit or select next question.
+	private void setAnswerScene(boolean correctAnswer) throws Exception{
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		Scene answerScene = new Scene(grid, 400, 275);
+		Text correctAnswerTitle;
+		// If the user selected the correct answer:
+		if(correctAnswer){
+			user.incrementScore(question.getKey());
+			correctAnswerTitle = new Text("Your answer is correct.");
+		}else{
+			correctAnswerTitle = new Text("Incorrect.");
+		}
+		Text answerExplanation = new Text(question.getExplanation());
+		answerExplanation.wrappingWidthProperty().bind(answerScene.widthProperty().subtract(30));
+		correctAnswerTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(correctAnswerTitle, 2,0);
+		grid.add(answerExplanation, 2, 1);
+		stage.setScene(answerScene);
+		Button nextQuestionButton = new Button("Next Question");
+		Button quitButton = new Button("Quit");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BASELINE_CENTER);
+		hbBtn.getChildren().add(nextQuestionButton);
+		hbBtn.getChildren().add(quitButton);
+		grid.add(hbBtn, 2, 4);
+		// Set action to get next question
+		nextQuestionButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e){
+				try{
+					setGameScene();
+				}catch (Exception e1){
+					e1.printStackTrace();
+				}
+			}
+		});
+		// Set action to quit the program
+		quitButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e){
+				displayScores();
+			}
+		});
+			
+	}
+	
+	/**
+	 * When the user selects to "quit" the program, display an exit screen with their updated scores
+	 */
+	private void displayScores(){
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		Scene exitScene = new Scene(grid, 400, 275);
+		// Create a variable to store the scores and then append to the screen
+		StringBuilder scoresString = new StringBuilder();
+		for(String key: user.getScores().keySet()){
+			scoresString.append(key + ": " + user.getScores().get(key) + "\n");
+		}
+		Text scoresText = new Text(scoresString.toString());
+		grid.add(scoresText, 2,0);
+		Button quitButton = new Button("Exit");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BASELINE_CENTER);
+		hbBtn.getChildren().add(quitButton);
+		grid.add(hbBtn, 2, 4);
+		stage.setScene(exitScene);
+		// Set action to quit the program
+		quitButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e){
+				System.exit(0);
+			}
+		});
+	}
 				
 }
 
